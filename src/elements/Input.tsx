2db58fc6b20b4d51ae3ba1d1ca275c5853f013e0
zzinao <kjn0265@gmail.com>
2022-10-19 18:49:22 +0900

    feat: ui 컴포넌트 생성

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Input() {
+interface Props {
+    placeholder: string;
+    autoComplete: string;
+    disabled: boolean;
+    id: string;
+    value: string;
+    type: string;
+    _onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
+export default function Input(props: Props) {
+    const { type, placeholder, value, id, autoComplete, disabled, _onChange } =
+        props;
     return (
         <>
             <InputFiled
+                type={type}
+                value={value}
+                disabled={disabled}
+                autoComplete={autoComplete}
+                id={id}
+                placeholder={placeholder}
+                onChange={_onChange}
             />
         </>
     );
 }
 
+Input.defaultProps = {
+    type: "text",
+    disabled: false,
+    value: "",
+    autoComplete: "on",
+    id: "",
+    placeholder: "텍스트를 입력하세요",
+    _onChange: () => { },
+};
 
 const InputFiled = styled.input`
   border: 1px solid #ddd;
